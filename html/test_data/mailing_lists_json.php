0eeff94983d00eb729871661587e65196d941b0e
Shumail Mohy-ud-Din <shumailmohyuddin@gmail.com>
2016-04-11 01:43:42 +0500

    Test JSON for mailing_lists info page

---
+++ html/test_data/mailing_lists_json.php
@@ -0,0 +1,16 @@
+{
+  "count": 2,
+  "mailing_lists": [{
+    "list_name": "7diagonals-weekly-dev",
+    "description": "Regular weekly 7D developers newsletter",
+    "subscribers": 9,
+    "creation_date": "Wed, Feb 16, 2016 7:34 pm",
+    "mailing_list_collection_name": "list-7diagonals-weekly-dev-123123123"
+  }, {
+    "list_name": "NUST-UG-Students",
+    "description": "Mailing List of NUST Undergrad students",
+    "subscribers": 3129,
+    "creation_date": "Jan, Apr 06, 2016 1:24 pm",
+    "mailing_list_collection_name": "list-NUST-UG-Students-577292bc72fc9881h"
+  }]
+}
